4cd68d1c42b4c30281f5e9b14998ce526d3fe9ef
MaxQuant Computer <keiwebber3@gmail.com>
2023-04-19 12:46:02 -0600

    Last AA, No duplicate Pep, documentaiton

--- makeInclusionList.py
+++ makeInclusionList.py
@@ -5,8 +5,12 @@ import re
 #Change these each time
 FILE_PATH_AND_NAME = "TMT_HCD_Study_PSMs.txt"
 OUTPUT_FILE_NAME = "TMT_Inclusion_List.csv"
+
+#Compound names
 FIRST_EXTENSION = "_01"
 FULL_EXTENSION = "_02"
+
+#output assumptions
 ADDUCT = "+H" #Almost always true
 RT_WINDOW = 5 #Dr. Kelly says this is a good amount
 
@@ -38,17 +42,17 @@ InclusionList = pd.DataFrame({"Compound": [],
                               })
 Proteins_Included = []
 Proteins_Full = []
-Sequences_Included = []
+Peptides_Included = []
 
 for index, eachRow in AllData.iterrows():
     currentProtein = eachRow["Master Protein Accessions"]
     currentSequence = re.sub("\\]\\.","",eachRow["Annotated Sequence"])
-    currentSequence = re.sub("\\.\\[[A-z\\-\\+]\\]","",currentSequence)
+    currentSequence = re.sub("\\.\\[[a-z\\-\\+]\\]","",currentSequence)
     currentSequence = re.sub("[a-z]","",currentSequence)
     currentSequence = re.sub("\\[","",currentSequence)
-    if currentProtein not in Proteins_Included and currentSequence not in Sequences_Included:
+    if currentProtein not in Proteins_Included and currentSequence not in Peptides_Included:
         Proteins_Included.append(currentProtein)
-        Sequences_Included.append(currentSequence)
+        Peptides_Included.append(currentSequence)
         newRow = {"Compound": [str(currentProtein) + FIRST_EXTENSION],
                   "Formula": [currentSequence],
                   "Adduct": [ADDUCT],
@@ -59,8 +63,8 @@ for index, eachRow in AllData.iterrows():
                   "HCD Collision Energies (%)": [eachRow["NCE [%]"]]
                   }
         InclusionList = pd.concat([InclusionList, pd.DataFrame(newRow)], ignore_index = True)
-    elif currentProtein not in Proteins_Full and currentSequence not in Sequences_Included:
-        Sequences_Included.append(currentSequence)
+    elif currentProtein not in Proteins_Full and currentSequence not in Peptides_Included:
+        Peptides_Included.append(currentSequence)
         Proteins_Full.append(currentProtein)
         newRow = {"Compound": [str(currentProtein) + FULL_EXTENSION],
                   "Formula": [currentSequence],
